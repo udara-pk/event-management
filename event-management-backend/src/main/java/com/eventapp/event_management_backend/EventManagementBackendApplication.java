4dc4ba88b00b9912993fa77de329ff555646a5f9
udara-pk <53249224+udara-pk@users.noreply.github.com>
2025-07-06 15:53:47 +0530

    backend initial
    initial backend file

---
+++ event-management-backend/src/main/java/com/eventapp/event_management_backend/EventManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eventapp.event_management_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventManagementBackendApplication.class, args);
+	}
+
+}
---
+++ event-management-backend/src/test/java/com/eventapp/event_management_backend/EventManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventapp.event_management_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
